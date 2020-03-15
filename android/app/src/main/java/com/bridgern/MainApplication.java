731abd38d5067814c1fcd20cc4e189a2e762723c
Thiago Almeida <thiagoa.silva@outlook.com>
2020-03-15 02:22:03 -0300

    configured idwall sdk in android

--- android/app/src/main/java/com/bridgern/MainApplication.java
+++ android/app/src/main/java/com/bridgern/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import co.idwall.toolkit.IDwallToolkit;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -42,6 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    IDwallToolkit.getInstance().init(this, "auth_key");
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this); // Remove this line if you don't want Flipper enabled
   }
